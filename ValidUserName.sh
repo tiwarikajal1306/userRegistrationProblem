3ade1c7ee232db6f479c4e9343f70f25ba652ded
kajal <tiwarikajal1306@gmail.com>
2020-05-06 14:47:25 +0530

    added valid last name

--- ValidUserName.sh
+++ ValidUserName.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
-read name
-pattern="^[[:upper:]]{1}[[:lower:]]{3,}$"
-if [[ $name =~ $pattern ]]
+read -p "Enter first name" firstName
+firstNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}$"
+if [[ $firstName =~ $firstNamePattern ]]
 then
-	echo "Valid Name"
+	echo "Valid FirstName"
 else
-	echo "Invalid Name"
+	echo "Invalid FirstName"
 fi
+read -p "Enter last name" lastName
+lastNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}$"
+if [[ $lastName =~ $lastNamePattern ]]
+then
+        echo "Valid LastName"
+else
+        echo "Invalid LastName"
+fi
+
