9e1c0d2148724867daeb963dad644058248717e5
kajal <tiwarikajal1306@gmail.com>
2020-05-07 09:21:27 +0530

    Added valid user email

--- ValidUserName.sh
+++ ValidUserName.sh
@@ -15,4 +15,11 @@ then
 else
         echo "Invalid LastName"
 fi
-
+read -p "Enter emailid name" emailid
+emailidPattern="^[[:alnum:]]+(([._+-]*)[[:alnum:]]+)*@[[:alnum:]]+.[[:alpha:]]{2,5}([.][[:alpha:]]{2,3})*$"
+if [[ $emailid =~ $emailidPattern ]]
+then
+        echo "Valid email"
+else
+        echo "Invalid email"
+fi
--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,10 @@
-echo "Validation program started"
+#!/bin/bash -x
+read -p "Enter emailid name" emailid
+#emailidPattern="^[[:alnum:]]+(([._+-]*)[[:alnum:]]+)*@[[:alnum:]]+.[[:alpha:]]{2,5}([.][[:alpha:]]{2,3})$"
+emailidPattern="^[[:alnum:]]+(([._+-]*)[[:alnum:]]+)*@[[:alnum:]]+.[[:alpha:]]{2,5}([.][[:alpha:]]{2,3})*$"
+if [[ $emailid =~ $emailidPattern ]]
+then
+        echo "Valid email"
+else
+        echo "Invalid email"
+fi
